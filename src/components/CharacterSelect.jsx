352a0d5b14e263f4e0beaadd2b6dd2fe9b55cdda
bananabread08 <rmkfermin@gmail.com>
2021-06-21 12:15:00 +0800

    added styles via styled-components

--- src/components/CharacterSelect.jsx
+++ src/components/CharacterSelect.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import data from '../helpers/data';
+import styled from 'styled-components';
+
+const SelectionContainer = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  border: 1px solid black;
+`;
+
+const CharImg = styled.img`
+  height: 50px;
+  width: 50px;
+  opacity: 0.7;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
 
 export default function CharacterSelect() {
   //const [charData, setCharData] = useState(data);
   return (
-    <div>
+    <SelectionContainer>
       {data.map((char) => {
-        return <img alt={char.name} src={char.icon} key={char.name}></img>;
+        return (
+          <CharImg alt={char.name} src={char.icon} key={char.name}></CharImg>
+        );
       })}
-    </div>
+    </SelectionContainer>
   );
 }
